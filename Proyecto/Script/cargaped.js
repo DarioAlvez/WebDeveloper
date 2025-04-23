2ebf42d186512c1ffd6bd7f377c02cb97ab7a784
dalvezPsag <160765807+dalvezPsag@users.noreply.github.com>
2025-04-23 10:57:33 -0300

    resubida despues de clonar

--- Proyecto/Script/cargaped.js
+++
@@ -1,4 +0,0 @@
-// tuve que generar un archivo nuevo JS para la web de cargapedidos, si no rompia las imagenes del index
-
-const fecha = new Date();
-document.getElementById("fecha-actual").value = fecha.toJSON().slice(0,10);
