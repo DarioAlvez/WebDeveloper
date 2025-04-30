89a563cfd9052c2ffb159b0a671cbb8da1d3c19c
dalvezPsag <160765807+dalvezPsag@users.noreply.github.com>
2025-04-30 13:10:02 -0300

    se agrego con JS el boton de agregar campo como explico Manuel y con ayuda de google ja!

--- Proyecto/Script/cargaped.js
+++ Proyecto/Script/cargaped.js
@@ -2,3 +2,20 @@
 
 const fecha = new Date();
 document.getElementById("fecha-actual").value = fecha.toJSON().slice(0,10);
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const botonAgregar = document.getElementById("agregar-campo"); 
+    const contenedor = document.getElementById("contenedor-campos");
+  
+    botonAgregar.addEventListener("click", function () {
+      const campoOriginal = contenedor.querySelector(".cod-prod-cant");
+      const nuevoCampo = campoOriginal.cloneNode(true);
+  
+   
+      nuevoCampo.querySelectorAll("input").forEach(input => input.value = ""); //deja vacio los inputs
+  
+      contenedor.appendChild(nuevoCampo);
+    });
+  });
